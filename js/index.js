316caebd5aa3d7f6754b5d1d226c8413cddfd19a
Ricardo Rojas <rikimaru55@gmail.com>
2019-11-24 19:50:36 -0800

    Added materialize css, howler library and js

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const woodBlockBtn = document.getElementById('woodBlockBtn');
+const beepBtn = document.getElementById('beepBtn');
+
+const woodBlockSound = new Howl({
+    src: ['assets/woodblock.webm', 'assets/woodblock.mp3'],
+    volume: 1,
+    onend: enableButton(woodBlockBtn)
+});
+
+const beepSound = new Howl({
+    src: ['assets/beep.webm', 'assets/woodblock.mp3'],
+    volume: 1,
+    onend: enableButton(beepBtn)
+});
+
+function enableButton(element) {
+    element.classList.remove('disabled');
+}
+
+woodBlockBtn.addEventListener('click', function(){
+    woodBlockSound.play();
+});
+
+beepBtn.addEventListener('click', function(){
+    beepSound.play();
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const modalBtn = document.getElementById('modalBtn');
+//     const instances = M.Modal.init(modalBtn);
+//   });
+
+M.AutoInit();
